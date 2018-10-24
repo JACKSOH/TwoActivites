a31cfaac3a38549ca17496ff0d3593a72df9413a
anreh <anurehka@gmail.com>
2018-10-24 14:14:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/twoactivites/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.taruc.twoactivites;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textview2);
+        textView.setText(message);
+        mReply = findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View view) {
+
+    }
+}
